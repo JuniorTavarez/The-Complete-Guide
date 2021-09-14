9765241f849e53cddb94344de248722af62028de
juniortavarez <jtavarez8@gmail.com>
2021-09-14 18:32:22 -0400

    playing with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem date='October' title='Rents' amount='800' />
+      <ExpenseItem date='November' title='Car Insurace' amount='40' />
+      <ExpenseItem date='Feb' title='Medical' amount='160' />
     </div>
   );
 }
