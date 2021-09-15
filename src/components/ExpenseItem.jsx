13b5659eb889038e23cd2530bdc5f705e1823156
JuniorTavarez <Jtavarez8@gmail.com>
2021-09-14 22:59:31 -0400

    playing with components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,11 @@
+import ExpenseDate from './ExpenseDate';
+
 import './ExpenseItem.css';
 
 function ExpenseItem({ date, title, amount }) {
-  //   const expenseDate = new Date(2021, 2, 28);
-  //   const expenseTitle = 'Car Insurance';
-  //   const expenseAmount = 294.67;
-
   return (
     <div className='expense-item'>
-      <div>{date}</div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
